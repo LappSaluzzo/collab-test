470760cd1657fb4c2337b3e1ef4845fc0ea77556
cha <eve0110010010@gmail.com>
2024-03-25 15:19:05 +0000

    Add increment.c

---
+++ increment.c
@@ -0,0 +1,12 @@
+/* increment.c - Dumb program to increment input integer and print it to console */
+
+#include <stdio.h>
+
+int main(void)
+{
+    int d;
+    scanf("%d", &d);
+    printf("%d\n", d);
+
+    return 0;
+}
