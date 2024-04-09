2fb71b68a897ffe7dbdd12f57e9d427e54d2fb24
LappSaluzzo <164902333+LappSaluzzo@users.noreply.github.com>
2024-04-09 09:55:38 +0000

    Add C++ implementation of increment

---
+++ increment.cpp
@@ -0,0 +1,13 @@
+/* increment.c - Dumb program to increment input integer and print it to console */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int d;
+    cin >> d;
+    cout << d + 1 << endl;
+
+    return 0;
+}
